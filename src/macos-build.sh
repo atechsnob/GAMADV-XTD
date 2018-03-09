90b57ae6b738bc62bd26a99f6c0d8969e19f7de0
Ross Scroggs <ross.scroggs@gmail.com>
2018-03-09 07:16:11 -0800

    Fixed bug in gam update group sync when the command was of the following form:
    ```
    gam update group csvkmd groups.csv keyfield group matchfield role MEMBER datafield email sync member csvdata email
    ```

--- src/macos-build.sh
+++ src/macos-build.sh
@@ -1,7 +1,7 @@
 rm -rf gam
 rm -rf build
 rm -rf dist
-rm -rf gamadv-xtd-$1-macos.tar.xz
+rm -f gamadv-xtd-$1-macos.tar
 
 /Library/Frameworks/Python.framework/Versions/2.7/bin/pyinstaller --clean -F --distpath=gam macos-gam.spec
 cp LICENSE gam
@@ -9,4 +9,4 @@ cp license.rtf gam
 cp whatsnew.txt gam
 cp Gam*.txt gam
 
-tar cfJ gamadv-xtd-$1-macos.tar.xz gam/ 
+tar -cf gamadv-xtd-$1-macos.tar gam/ 
