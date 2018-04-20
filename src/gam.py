f9a6d45c743d09827a69069a0ee5e35f45c31c40
Ross Scroggs <ross.scroggs@gmail.com>
2018-04-20 15:59:47 -0700

    Code cleanup

--- src/gam.py
+++ src/gam.py
@@ -28345,7 +28345,7 @@ def _printShowTeamDrives(users, csvFormat, useDomainAdminAccess):
           if formatJSON:
             row = {u'User': user, u'id': teamdrive[u'id'], u'name': teamdrive[u'name']}
             if not useDomainAdminAccess:
-              row[u'role'] = teamdrive.pop(u'role')
+              row[u'role'] = teamdrive[u'role']
             row[u'JSON'] = json.dumps(cleanJSON(teamdrive, u'', timeObjects=TEAMDRIVE_TIME_OBJECTS), ensure_ascii=False, sort_keys=True)
             csvRows.append(row)
           else:
